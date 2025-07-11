f87c2cf982004cbfa09b2465eae6bba01a363dbb
Pavlo <marlana08022022@gmail.com>
2025-07-11 21:44:49 +0300

    change sort and obg

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write code here
+const list = document.querySelector('ul');
+
+function sortList(sort) {
+  const mas = Array.from(sort.querySelectorAll('li'));
+
+  mas.sort((a, b) => {
+    return Number(b.dataset.salary) - Number(a.dataset.salary);
+  });
+
+  sort.innerHTML = '';
+
+  mas.forEach((m) => {
+    list.appendChild(m);
+  });
+}
+
+function getEmployees(emp) {
+  const items = emp.querySelectorAll('li');
+
+  return Array.from(items).map((item) => ({
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: Number(item.dataset.age),
+  }));
+}
+
+sortList(list);
+
+getEmployees(list);
